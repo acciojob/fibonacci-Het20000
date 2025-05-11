7d1969db7d5721ec8fcca3a7bdadc7e6c5751f9b
Het20000 <hp985383@gmail.com>
2025-05-11 11:14:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 function fibonacci(num) {
-  if (num === 0) return 0;
-  if (num === 1) return 0;
-  if (num === 2) return 1;
+    if (num === 0) return 0;
+    if (num === 1) return 0;
+    if (num === 2) return 1;
 
-  let a = 0, b = 1, temp;
-  for (let i = 3; i <= num; i++) {
-    temp = a + b;
-    a = b;
-    b = temp;
-  }
-  return b;
-}
-
-function runFibonacci() {
-  const input = parseInt(document.getElementById("fibInput").value);
-  const result = fibonacci(input);
-  document.getElementById("fibResult").innerText = "Fibonacci Number: " + result;
+    let a = 0, b = 1;
+    for (let i = 3; i <= num; i++) {
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+    return b;
 }
